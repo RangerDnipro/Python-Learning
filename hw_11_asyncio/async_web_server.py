d8da1ce962cd4bfda676e99cea7a8e8b7ed7bd27
Данило Кочешвілі <danil@ukr.net>
2024-10-24 20:13:41 +0300

    Update async_web_server.py

--- hw_11_asyncio/async_web_server.py
+++ hw_11_asyncio/async_web_server.py
@@ -6,20 +6,22 @@ import asyncio
 from aiohttp import web, ClientSession
 
 
-async def handle_root(request: web.Request) -> web.Response:
+async def handle_root(request: web.Request) -> web.Response:  # pylint: disable=unused-argument
     """
     Обробляємо маршрут /, повертаючи простий текст "Hello, World!"
-    :param request: Запит до сервера
+    :param request: Запит до сервера, аргумент необхідний для того,
+    щоб функція могла відповідати вимогам бібліотеки aiohttp і отримувати запити
     :return: Відповідь з текстом "Hello, World!"
     """
     return web.Response(text="Hello, World!")
 
 
-async def handle_slow(request: web.Request) -> web.Response:
+async def handle_slow(request: web.Request) -> web.Response:  # pylint: disable=unused-argument
     """
     Обробляємо маршрут /slow, імітуючи довгу операцію
     з затримкою в 5 секунд і повертаючи текст "Operation completed"
-    :param request: Запит до сервера
+    :param request: Запит до сервера, аргумент необхідний для того,
+    щоб функція могла відповідати вимогам бібліотеки aiohttp і отримувати запити
     :return: Відповідь з текстом "Operation completed" після затримки в 5 секунд
     """
     await asyncio.sleep(5)
