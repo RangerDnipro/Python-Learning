bc237414a8f406b07e34cd5c5831ad433894b74d
Данило Кочешвілі <danil@ukr.net>
2024-11-16 03:24:35 +0200

    Add files via upload

---
+++ hw_17_forms_in_django/user_management/user_management/urls.py
@@ -0,0 +1,37 @@
+"""
+URL configuration for user_management project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from accounts import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # Посилання на представлення для головної сторінки
+    path('', views.home_view, name='home'),
+    # Включаємо маршрути з додатка 'accounts'
+    path('accounts/', include('accounts.urls')),
+    # Додаємо стандартні маршрути авторизації
+    path('accounts/', include('django.contrib.auth.urls')),
+    # URL-патерни проєкту
+    path('', include('accounts.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
